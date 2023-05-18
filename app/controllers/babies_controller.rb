b5fe5591383e380c83de3274abb0545a6f9efb47
Jamie Ewing <jamesewing0@gmail.com>
2023-05-18 21:19:20 +0100

    baby form done and saves

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -2,4 +2,20 @@ class BabiesController < ApplicationController
   def index
     @babies = Baby.all
   end
+
+  def new
+    @baby = Baby.new
+  end
+
+  def create
+    @baby = Baby.new(baby_params)
+    @baby.save
+    redirect_to babies_path(@baby)
+  end
+
+  private
+
+  def baby_params
+    params.require(:baby).permit(:age, :name, :potty_trained, :avg_napping_hours, :screamometer, :medical_needs)
+  end
 end

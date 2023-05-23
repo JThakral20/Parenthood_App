e7551fe3cbd09b70864a79c2e0599ec10f454e85
Belen Martinez <belenmgil@gmail.com>
2023-05-23 21:25:29 +0100

    validating first_name on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :babies, through: :bookings
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
 end

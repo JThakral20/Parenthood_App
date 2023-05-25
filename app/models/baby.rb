e0e8730b4d63686a07c7952718d270c80582284f
Jamie Ewing <jamesewing0@gmail.com>
2023-05-25 20:19:41 +0100

    map added

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -2,4 +2,7 @@ class Baby < ApplicationRecord
   has_many :bookings
   validates :name, presence: true
   validates :age, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
